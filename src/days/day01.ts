3431dcb84c2502824955528d4789868caeabd2cd
raincycles <143883317+raincycles@users.noreply.github.com>
2023-12-17 12:21:36 +0100

    Refactor helper functions in day 1

--- src/days/day01.ts
+++ src/days/day01.ts
@@ -2,22 +2,22 @@ function isDigit(ch: string): boolean {
   return ch >= "0" && ch <= "9";
 }
 
-function getLastDigit(source: string): string {
+function getLastDigit(source: string): number {
   for (let i = source.length - 1; i >= 0; i--) {
     const ch = source[i];
 
     if (isDigit(ch)) {
-      return ch;
+      return Number.parseInt(ch, 10);
     }
   }
 
   throw new Error("Unreachable");
 }
 
-function getFirstDigit(source: string): string {
+function getFirstDigit(source: string): number {
   for (const ch of source) {
     if (isDigit(ch)) {
-      return ch;
+      return Number.parseInt(ch, 10);
     }
   }
 
@@ -31,7 +31,7 @@ export function part1(input: string): number {
   for (const ln of lines) {
     const firstDigit = getFirstDigit(ln);
     const lastDigit = getLastDigit(ln);
-    sum += Number.parseInt(firstDigit + lastDigit, 10);
+    sum += firstDigit * 10 + lastDigit;
   }
 
   return sum;
