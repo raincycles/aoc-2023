eb7f7a0ea20ccd30fe24aaa7a3d0daecb20a5e52
raincycles <143883317+raincycles@users.noreply.github.com>
2023-12-17 09:53:21 +0100

    Initial commit

---
+++ src/days/day01.ts
@@ -0,0 +1,7 @@
+export function part1(_input: string): number {
+  throw new Error("Unimplemented");
+}
+
+export function part2(_input: string): number {
+  throw new Error("Unimplemented");
+}
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import * as fs from "node:fs";
+import * as day from "./days/day01";
+
+if (process.argv.length < 3) {
+  console.error("Please provide a path to the input file.");
+  process.exit(1);
+}
+
+let input = "";
+
+try {
+  const inputPath = process.argv[2];
+  input = fs.readFileSync(inputPath, "utf-8");
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
+
+const solution = day.part1(input);
+console.log(solution);
