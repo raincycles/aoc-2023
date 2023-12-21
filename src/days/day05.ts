bfff3b8e4b1387bfd399b733ef42ebebc91dfac8
raincycles <143883317+raincycles@users.noreply.github.com>
2023-12-21 20:23:27 +0100

    Add solution for part 2 of day 5

--- src/days/day05.ts
+++ src/days/day05.ts
@@ -42,3 +42,73 @@ export function part1(input: string): number {
 
   return Math.min.apply(null, mappedValues);
 }
+
+type Range = [from: number, to: number];
+
+export function part2(input: string): number {
+  const lines = input.split("\n");
+  const sections: Mapping[][] = [];
+
+  let current = 3;
+  while (current < lines.length) {
+    const mappings: Mapping[] = [];
+
+    while (current < lines.length && lines[current].length > 0) {
+      const chunks = lines[current].split(" ");
+      const values = chunks.map((x) => Number.parseInt(x, 10));
+      mappings.push(values as Mapping);
+      current++;
+    }
+
+    sections.push(mappings);
+    current += 2;
+  }
+
+  const chunks = lines[0].substring(7).split(" ");
+  const seedValues = chunks.map((x) => Number.parseInt(x, 10));
+
+  let ranges: Range[] = [];
+  for (let i = 0; i < seedValues.length; i += 2) {
+    const from = seedValues[i];
+    const length = seedValues[i + 1];
+    ranges.push([from, from + length]);
+  }
+
+  for (const mappings of sections) {
+    const newRanges: Range[] = [];
+
+    while (ranges.length > 0) {
+      const [from, to] = ranges.pop()!;
+      let hadOverlap = false;
+
+      for (const [dst, src, len] of mappings) {
+        const overFrom = Math.max(from, src);
+        const overTo = Math.min(to, src + len);
+
+        if (overFrom >= overTo) {
+          continue;
+        }
+
+        const delta = dst - src;
+        newRanges.push([overFrom + delta, overTo + delta]);
+        hadOverlap = true;
+
+        if (overFrom > from) {
+          ranges.push([from, overFrom]);
+        }
+
+        if (overTo < to) {
+          ranges.push([overTo, to]);
+        }
+      }
+
+      if (!hadOverlap) {
+        newRanges.push([from, to]);
+      }
+    }
+
+    ranges = newRanges;
+  }
+
+  return ranges.reduce((acc, x) => Math.min(acc, x[0]), Infinity);
+}
