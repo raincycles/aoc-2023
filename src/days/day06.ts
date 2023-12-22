3457890eadede34a82cb017fa18703d97d821fa9
raincycles <143883317+raincycles@users.noreply.github.com>
2023-12-22 12:25:06 +0100

    Add solution for part 1 of day 6

---
+++ src/days/day06.ts
@@ -0,0 +1,49 @@
+function isDigit(ch: string): boolean {
+  return ch >= "0" && ch <= "9";
+}
+
+export function part1(input: string): number {
+  const lines = input.split("\n");
+  const sections: number[][] = [];
+  let acc = 1;
+
+  for (const ln of lines) {
+    const section: number[] = [];
+
+    let current = 10;
+    while (current < ln.length) {
+      if (!isDigit(ln[current])) {
+        current++;
+        continue;
+      }
+
+      const start = current;
+      let end = current + 1;
+      while (end < ln.length && isDigit(ln[end])) {
+        end++;
+      }
+
+      const value = Number.parseInt(ln.substring(start, end), 10);
+      section.push(value);
+      current = end;
+    }
+
+    sections.push(section);
+  }
+
+  const times = sections[0];
+  const distances = sections[1];
+
+  for (let i = 0; i < times.length; i++) {
+    const b = -times[i];
+    const c = distances[i] + 1;
+
+    const dSqrt = Math.sqrt(Math.pow(b, 2) - 4 * c);
+    const x1 = (-b - dSqrt) / 2;
+    const x2 = (-b + dSqrt) / 2;
+
+    acc *= Math.floor(x2) - Math.ceil(x1) + 1;
+  }
+
+  return acc;
+}
